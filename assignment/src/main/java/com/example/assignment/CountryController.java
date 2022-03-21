4a59691826a093456a0c4002050657e04c24fe81
katazorkina <katazorkina@gmail.com>
2022-03-21 20:57:39 +0200

    Changed allowed headers

--- assignment/src/main/java/com/example/assignment/CountryController.java
+++ assignment/src/main/java/com/example/assignment/CountryController.java
@@ -12,7 +12,7 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class CountryController {
 
